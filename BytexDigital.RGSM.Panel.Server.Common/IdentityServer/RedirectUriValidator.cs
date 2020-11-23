c952792efd74637b11c975da1b2fcd03c5c93e1b
Ryan Torzynski <ryantorzynski@gmail.com>
2020-11-23 10:30:44 +0100

    Made IdentityServer approve all urls in development

--- BytexDigital.RGSM.Panel.Server.Common/IdentityServer/RedirectUriValidator.cs
+++ BytexDigital.RGSM.Panel.Server.Common/IdentityServer/RedirectUriValidator.cs
@@ -4,21 +4,27 @@ using System.Threading.Tasks;
 using IdentityServer4.Models;
 using IdentityServer4.Validation;
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
 namespace BytexDigital.RGSM.Panel.Server.Common.IdentityServer
 {
     public class RedirectUriValidator : IRedirectUriValidator
     {
         private readonly HttpContext _httpContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public RedirectUriValidator(IHttpContextAccessor httpContext)
+        public RedirectUriValidator(IHttpContextAccessor httpContext, IWebHostEnvironment webHostEnvironment)
         {
             _httpContext = httpContext.HttpContext;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public Task<bool> IsPostLogoutRedirectUriValidAsync(string requestedUri, Client client)
         {
+            if (_webHostEnvironment.IsDevelopment()) return Task.FromResult(true);
+
             if (requestedUri.StartsWith("http"))
             {
                 try
@@ -45,6 +51,8 @@ namespace BytexDigital.RGSM.Panel.Server.Common.IdentityServer
 
         public Task<bool> IsRedirectUriValidAsync(string requestedUri, Client client)
         {
+            if (_webHostEnvironment.IsDevelopment()) return Task.FromResult(true);
+
             if (requestedUri.StartsWith("http"))
             {
                 try
