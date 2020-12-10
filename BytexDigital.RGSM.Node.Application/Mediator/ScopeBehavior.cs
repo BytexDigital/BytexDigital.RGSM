c664cc0fa3663c35410d0a4049300b8281d2c3a8
Ryan Torzynski <ryantorzynski@gmail.com>
2020-12-10 13:39:44 +0100

    Fixed ScopeBehavior not properly running subsequent code in the scope

--- BytexDigital.RGSM.Node.Application/Mediator/ScopeBehavior.cs
+++ BytexDigital.RGSM.Node.Application/Mediator/ScopeBehavior.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 
+using Autofac;
+
 using BytexDigital.RGSM.Node.Application.Core;
 
 using MediatR;
@@ -10,15 +12,23 @@ namespace BytexDigital.RGSM.Node.Application.Mediator
     public class ScopeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
         private readonly ScopeService _scopeService;
+        private readonly ILifetimeScope _lifetimeScope;
 
-        public ScopeBehavior(ScopeService scopeService)
+        public ScopeBehavior(ScopeService scopeService, ILifetimeScope lifetimeScope)
         {
             _scopeService = scopeService;
+            _lifetimeScope = lifetimeScope;
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            return await _scopeService.RunScoped(() => next.Invoke());
+            //return await _scopeService.RunScoped(() => next.Invoke());
+            if (_lifetimeScope.Tag == (object)"Internal") return await next.Invoke();
+
+            using (var scope = _lifetimeScope.BeginLifetimeScope((object)"Internal"))
+            {
+                return (TResponse)await scope.Resolve<IMediator>().Send(request, cancellationToken);
+            }
         }
     }
 }
