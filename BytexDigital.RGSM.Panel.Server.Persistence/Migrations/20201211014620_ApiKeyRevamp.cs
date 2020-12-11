d30199f34196eb85ca7256b7a1e4afe1e31e56e2
Ryan Torzynski <ryantorzynski@gmail.com>
2020-12-11 03:37:40 +0100

    Master DbContext changes for api keys

---
+++ BytexDigital.RGSM.Panel.Server.Persistence/Migrations/20201211014620_ApiKeyRevamp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BytexDigital.RGSM.Panel.Server.Persistence.Migrations
+{
+    public partial class ApiKeyRevamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NodeKeys");
+
+            migrationBuilder.CreateTable(
+                name: "ApiKeys",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    NodeId = table.Column<string>(type: "TEXT", nullable: true),
+                    Value = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeCreated = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Version = table.Column<byte[]>(type: "BLOB", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiKeys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApiKeys_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiKeys_NodeId",
+                table: "ApiKeys",
+                column: "NodeId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiKeys");
+
+            migrationBuilder.CreateTable(
+                name: "NodeKeys",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    ApiKey = table.Column<string>(type: "TEXT", nullable: false),
+                    NodeId = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeCreated = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Version = table.Column<byte[]>(type: "BLOB", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NodeKeys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NodeKeys_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NodeKeys_NodeId",
+                table: "NodeKeys",
+                column: "NodeId",
+                unique: true);
+        }
+    }
+}
