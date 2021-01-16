43b156eb044f5bf0023f137a8092b1e0d726f315
Ryan Torzynski <ryantorzynski@gmail.com>
2021-01-17 00:23:55 +0100

    Added listing of accounts

--- BytexDigital.RGSM.Panel/Client/Pages/Settings/Accounts/Index.razor.cs
+++ BytexDigital.RGSM.Panel/Client/Pages/Settings/Accounts/Index.razor.cs
@@ -1,9 +1,30 @@
 ﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using BytexDigital.RGSM.Panel.Client.Common.Core;
+using BytexDigital.RGSM.Panel.Server.TransferObjects.Entities;
+
 using Microsoft.AspNetCore.Components;
 
 namespace BytexDigital.RGSM.Panel.Client.Pages.Settings.Accounts
 {
     public partial class Index : ComponentBase
     {
+        [Inject]
+        public AccountService AccountService { get; set; }
+
+        public List<ApplicationUserDto> ApplicationUsers { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshAsync();
+            StateHasChanged();
+        }
+
+        public async Task RefreshAsync()
+        {
+            ApplicationUsers = await AccountService.GetApplicationUsersAsync();
+        }
     }
 }
